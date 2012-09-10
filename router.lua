afec1c0dd41aa893b3b9699b742a12288456d4bc
Tim Caswell <tim@creationix.com>
2012-09-10 18:09:14 +0000

    Add basic routing

--- router.lua
+++ router.lua
@@ -3,13 +3,18 @@ return function (app, setup)
 
   local router = {}
   function router.get(path, handler)
-
+    routes[#routes + 1] = {path, handler}
   end
 
   setup(router)
 
   return function (req, res)
-    console.log("TODO: Implement router")
+    for i, pair in ipairs(routes) do
+      local path, handler = unpack(pair)
+      if req.url.path == path then
+        return handler(req, res)
+      end
+    end
     app(req, res)
   end
 end
