4c7467dcb5f1afb09f1a06d165746c0a12fc74a0
Tim Caswell <tim@creationix.com>
2012-09-10 13:45:50 -0700

    Implement pattern matching

--- router.lua
+++ router.lua
@@ -3,6 +3,19 @@ return function (app, setup)
 
   local router = {}
   function router.get(path, handler)
+    if path:match("/:") then
+      local pattern = path:gsub("/(:[a-z]+)", "/([^/]+)")
+      local names = {path:match("/:([a-z]+)")}
+      path = function (path)
+        local matches = {path:match(pattern)}
+        if #matches == 0 or not matches[1] then return end
+        local params = {}
+        for i, name in ipairs(names) do
+          params[name] = matches[i]
+        end
+        return params
+      end
+    end
     routes[#routes + 1] = {path, handler}
   end
 
@@ -11,7 +24,13 @@ return function (app, setup)
   return function (req, res)
     for i, pair in ipairs(routes) do
       local path, handler = unpack(pair)
-      if req.url.path == path then
+      if type(path) == "function" then
+        local matches = path(req.url.path)
+        if matches then
+          req.params = matches
+          return handler(req, res)
+        end
+      elseif req.url.path == path then
         return handler(req, res)
       end
     end
